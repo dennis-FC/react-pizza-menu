dcc4ec277db3ee4788bbeae930d1c841156d62b3
dennisFC <dennisyes25@gmail.com>
2023-09-17 15:10:26 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,138 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast Reat Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}.We're currently open</footer>
+  );
+  //return React.createElement("footer", null, "We're currently open");
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinact" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//React before 18
+//React.render(<App />);
+
+const uri = "https://task-manager-ro25.onrender.com/tasks";
+const ttest = async () => {
+  await fetch(uri, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json(); // 使用 text() 可以得到純文字 String
+    })
+    .catch((e) => {
+      console.log();
+    });
+};
+
+ttest();
+
+// const uri = "https://task-manager-ro25.onrender.com/users";
+// const ttest = async () => {
+//   await fetch(uri, {
+//     method: "POST",
+//     headers: new Headers({
+//       "Content-Type": "text/json",
+//     }),
+//     body: {
+//       name: "TT",
+//       email: "TT@gmail.com",
+//       password: "123456789",
+//     },
+//   })
+//     .then((res) => {
+//       return res; // 使用 text() 可以得到純文字 String
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//     });
+// };
+
+// ttest();
