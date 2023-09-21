893ae098d9e65812e1acc8539dddc4ee22392b66
dennis_new <dennisyes25@gmail>
2023-09-21 22:49:12 +0800

    class 43

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,35 +58,70 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast Reat Pizza Co.</h1>;
+  //const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style} className="header">
+        Fast Reat Pizza Co.
+      </h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato,mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log([props]);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're close");
+
   return (
-    <footer>{new Date().toLocaleTimeString()}.We're currently open</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We're currently open
+    </footer>
   );
   //return React.createElement("footer", null, "We're currently open");
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinact" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 // React v18
@@ -99,39 +135,16 @@ root.render(
 //React before 18
 //React.render(<App />);
 
-const uri = "https://task-manager-ro25.onrender.com/tasks";
-const ttest = async () => {
-  await fetch(uri, {
-    method: "GET",
-  })
-    .then((res) => {
-      return res.json(); // 使用 text() 可以得到純文字 String
-    })
-    .catch((e) => {
-      console.log();
-    });
-};
-
-ttest();
-
-// const uri = "https://task-manager-ro25.onrender.com/users";
+// const uri = "https://task-manager-ro25.onrender.com/tasks";
 // const ttest = async () => {
 //   await fetch(uri, {
-//     method: "POST",
-//     headers: new Headers({
-//       "Content-Type": "text/json",
-//     }),
-//     body: {
-//       name: "TT",
-//       email: "TT@gmail.com",
-//       password: "123456789",
-//     },
+//     method: "GET",
 //   })
 //     .then((res) => {
-//       return res; // 使用 text() 可以得到純文字 String
+//       return res.json(); // 使用 text() 可以得到純文字 String
 //     })
 //     .catch((e) => {
-//       console.log(e);
+//       console.log();
 //     });
 // };
 
